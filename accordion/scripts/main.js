6d9ee9a36153cc8566cf103b962dbf5e9cb64c19
Pedro Martin <pataruco@gmail.com>
2019-08-26 21:46:38 +0100

    Toggle working

--- accordion/scripts/main.js
+++ accordion/scripts/main.js
@@ -1,16 +1,33 @@
-const accordionTitles = document.querySelectorAll('dt');
+const accordionTitles = Array.from(document.querySelectorAll('dt'));
 
 accordionTitles.forEach(accordionTitle =>
   accordionTitle.addEventListener('click', openAccordion),
 );
 
-function closeAllAccordions() {
-  accordionTitles.forEach(accordionTitle =>
+function closeAllAccordionsExcept(element) {
+  const index = accordionTitles.indexOf(element);
+  const filteredAccordionTitles = accordionTitles.filter(
+    (accordionTitle, i) => {
+      if (i !== index) {
+        return accordionTitle;
+      }
+    },
+  );
+
+  filteredAccordionTitles.forEach(accordionTitle =>
     accordionTitle.classList.remove('accordion-open'),
   );
 }
 
 function openAccordion(event) {
-  closeAllAccordions();
-  event.target.classList.toggle('accordion-open');
+  const currentTarget = event.currentTarget;
+
+  let target;
+  if (currentTarget.nodeName === 'DT') {
+    target = currentTarget;
+  } else {
+    target = currentTarget.parentNode;
+  }
+  closeAllAccordionsExcept(target);
+  return target.classList.toggle('accordion-open');
 }
